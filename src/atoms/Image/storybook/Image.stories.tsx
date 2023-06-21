a5f2d76349c7456d2d0e61e1f103d2d486252f67
Chai Landau <chai.landau@gmail.com>
2023-06-20 20:52:48 -0400

    refactor: Remove unecessary type from Image SB

--- src/atoms/Image/storybook/Image.stories.tsx
+++ src/atoms/Image/storybook/Image.stories.tsx
@@ -1,5 +1,4 @@
 import OutdoorTents from '@storybookAssets/images/outdoor_tents.jpg';
-import { ImageProps as NextImageProps } from 'next/image';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -31,7 +30,7 @@ type Story = StoryObj<typeof Image>;
 
 export const Default: Story = {
     args: {
-        src: OutdoorTents as NextImageProps,
+        src: OutdoorTents,
         alt: 'Camo tents outdoors next to table with laser tag equipment',
         hasBorder: true,
         priority: true,
