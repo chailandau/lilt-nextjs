604bd576f49b18a2e9b381d1aa712bbc97a4fc1b
Chai Landau <chai.landau@gmail.com>
2023-06-24 20:46:15 -0400

    feat: Add snapshots

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -2,28 +2,44 @@
 
 import { composeStory } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 import Meta, { Default, Green, White, WithLink } from './storybook/Button.stories';
 
-const getStorybookInfo = (Variant: typeof Default, role?: string) => ({
+const getButtonInfo = (Variant: typeof Default, role?: string) => ({
     variant: composeStory(Variant, Meta),
-    text: Variant.args?.children as string,
-    role: role || 'button'
+    text: Variant.args?.children || '',
+    role: role || 'button',
+    color: Variant.args?.color || '',
 });
 
 const buttonVariants = [
-    getStorybookInfo(Default),
-    getStorybookInfo(Green),
-    getStorybookInfo(White),
-    getStorybookInfo(WithLink, 'link')
+    getButtonInfo(Default),
+    getButtonInfo(Green),
+    getButtonInfo(White),
+    getButtonInfo(WithLink, 'link')
 ];
 
-buttonVariants.forEach(({ variant: Button, text, role = 'button' }) => {
+buttonVariants.forEach(({ variant: Button, text, color, role }) => {
     describe(text, () => {
-        it('should render', () => {
+        if (text === 'Default button') {
+            it('has default color', () => {
+                render(<Button color={undefined} />);
+                expect(screen.getByRole(role)).toHaveClass(color);
+            });
+        }
+        it('renders', () => {
             render(<Button />);
             expect(screen.getByRole(role)).toHaveTextContent(text);
         });
+        it('has correct class', () => {
+            render(<Button />);
+            expect(screen.getByRole(role)).toHaveClass(color);
+        }),
+            it('matches snapshot', () => {
+                const tree = renderer.create(<Button />).toJSON();
+                expect(tree).toMatchSnapshot();
+            });
     });
 });
 
