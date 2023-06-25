9a29c593ec082ac3674f19863b3d2d37802f870e
Chai Landau <chai.landau@gmail.com>
2023-06-24 21:01:57 -0400

    feat: Add basic accessibility testing

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -2,6 +2,7 @@
 
 import { composeStory } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import renderer from 'react-test-renderer';
 
 import Meta, { Default, Green, White, WithLink } from './storybook/Button.stories';
@@ -22,6 +23,7 @@ const buttonVariants = [
 
 buttonVariants.forEach(({ variant: Button, text, color, role }) => {
     describe(text, () => {
+        expect.extend(toHaveNoViolations);
         if (text === 'Default button') {
             it('has default color', () => {
                 render(<Button color={undefined} />);
@@ -35,11 +37,15 @@ buttonVariants.forEach(({ variant: Button, text, color, role }) => {
         it('has correct class', () => {
             render(<Button />);
             expect(screen.getByRole(role)).toHaveClass(color);
-        }),
-            it('matches snapshot', () => {
-                const tree = renderer.create(<Button />).toJSON();
-                expect(tree).toMatchSnapshot();
-            });
+        });
+        it('passes basic accessibility test', async () => {
+            const { container } = render(<Button />);
+            expect(await axe(container)).toHaveNoViolations();
+        });
+        it('matches snapshot', () => {
+            const buttonTree = renderer.create(<Button />).toJSON();
+            expect(buttonTree).toMatchSnapshot();
+        });
     });
 });
 
