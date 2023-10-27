69f19777842e921c16ff1da3c6fb40112d5f388f
Chai Landau <chai.landau@gmail.com>
2023-10-26 20:14:45 -0400

    style: lint

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -10,8 +10,9 @@ export const isSvg = (sourceUrl: string): boolean => sourceUrl.endsWith('.svg');
  * Gets the placeholder value based on the presence of an SVG image.
  * If `svgImg` is `true`, returns `undefined`.
  * If `svgImg` is `false`, returns `'blur'`.
- * 
+ *
  * @param {boolean} svgImg - Indicates whether an SVG image is present.
  * @returns {undefined | 'blur'} - The placeholder value.
  */
-export const getPlaceholder = (svgImg: boolean): undefined | 'blur' => svgImg ? undefined : 'blur';
+export const getPlaceholder = (svgImg: boolean): undefined | 'blur' =>
+    svgImg ? undefined : 'blur';
