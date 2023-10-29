684da42143672b85f5fe9ffee6a592f2154b0261
Chai Landau <112015853+chailandau@users.noreply.github.com>
2023-10-29 19:48:24 -0400

    feat: add custom 404 (#66)

--- src/templates/404/404.tsx
+++ src/templates/404/404.tsx
@@ -1,5 +1,23 @@
-import Heading from '@/atoms/Heading';
+import styles from './404.module.scss';
 
-const NotFound = () => <Heading>Page not found</Heading>;
+import LaserTarget from '@/assets/svg/LaserTarget.svg';
+import Heading from '@/atoms/Heading';
+import Image from '@/atoms/Image';
+import Section from '@/molecules/Section';
+
+const NotFound = () => (
+    <Section className={styles['not-found']}>
+        <Image
+            src={LaserTarget}
+            alt='Laser tag target'
+            hasBorder={false}
+            className={styles['not-found__image']}
+        />
+        <Heading size='xl'>Target out of range</Heading>
+        <Heading color='blue' size='sm' as='p'>
+            Page not found
+        </Heading>
+    </Section>
+);
 
 export default NotFound;
