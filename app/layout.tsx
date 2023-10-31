b6ea701b580cba948cb2b1bfe4a6926ae02b6c37
Chai Landau <112015853+chailandau@users.noreply.github.com>
2023-10-31 19:51:20 -0400

    feat: add google analytics (#91)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import { Metadata } from 'next';
+import Script from 'next/script';
 
 import { FOOTER_QUERY, HEADER_QUERY } from '@/api/graphqlQueries';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header/Header';
 import BackToTop from '@/molecules/BackToTop';
 import { getData } from '@/utils/getData';
+
 import '@/assets/fonts/fonts.css';
 import '@/styles/main.scss';
 
@@ -22,6 +24,23 @@ export default async function RootLayout({
 
     return (
         <html lang='en'>
+            <Script
+                strategy='afterInteractive'
+                src='https://www.googletagmanager.com/gtag/js?id=G-7Y1YQ9834N'
+            />
+            <Script
+                id='google-analytics'
+                strategy='afterInteractive'
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-SPPSLWSVHZ');
+                `
+                }}
+            />
             <body>
                 <Header
                     menuItems={HeaderData?.menuItems || null}
